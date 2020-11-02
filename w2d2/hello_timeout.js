6a717f42f6f77c9c032173ddd0485e87134d21e3
Sara Irving <sarakirving@gmail.com>
2020-11-02 22:42:48 +0000

    print each work 1 sec apart

--- w2d2/hello_timeout.js
+++ w2d2/hello_timeout.js
@@ -1,3 +1,12 @@
-setTimeout(() => {
-  console.log("4311o th3r3 w0r1d");
-}, 3000)
+// setTimeout(() => {
+//   console.log("4311o th3r3 w0r1d");
+// }, 3000)
+
+const words = "4311o th3r3 w0r1d".split(" ");
+//console.log(words);
+
+for (let i = 0; i < words.length; i++) {
+  setTimeout(() => {
+    console.log(words[i]);
+  }, 1000 + (i * 1000))
+}
