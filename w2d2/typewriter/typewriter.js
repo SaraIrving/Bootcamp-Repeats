9f3440946fb482054dacd5c19747c375ece90943
Sara Irving <sarakirving@gmail.com>
2020-11-02 23:01:42 +0000

    add newline at the end of the animation so next time prompt appears on it's own line

--- w2d2/typewriter/typewriter.js
+++ w2d2/typewriter/typewriter.js
@@ -1,8 +1,14 @@
 const sentence = "hello there from lighthouse labs";
 
 for (let i = 0; i < sentence.length; i++) {
-  setTimeout(() => {
-    process.stdout.write(sentence[i]);
-  }, 50 * i);
-  
+
+  if (i === sentence.length - 1) {
+    setTimeout(() => {
+      process.stdout.write(sentence[i] + "\n");
+    }, 50 * i);
+  } else {
+    setTimeout(() => {
+      process.stdout.write(sentence[i]);
+    }, 50 * i);
+  }
 }
