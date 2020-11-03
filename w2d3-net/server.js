74013d0123992f6d94e95c4878fd196eb3816c89
Sara Irving <sarakirving@gmail.com>
2020-11-03 02:04:37 +0000

    code to send a message to the client after they connect

--- w2d3-net/server.js
+++ w2d3-net/server.js
@@ -3,6 +3,11 @@ const net = require('net');
 
 const server = net.createServer();
 
+server.on('connection', (client) => {
+  console.log("New client connected!");
+  client.write("Hello there!")
+})
+
 server.listen(3000, () => {
   console.log("Server listening on port 3000!");
 });
