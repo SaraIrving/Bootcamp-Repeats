8204446abf1baf838fa6b559cc36477f4220c115
Sara Irving <sarakirving@gmail.com>
2020-11-04 20:33:11 +0000

    add console logs to visualize order of code execution

--- w3/w3d1/app.js
+++ w3/w3d1/app.js
@@ -4,12 +4,15 @@ const PORT = 8080;
 // a function which handles requests and sends response
 
 const requestHandler = function(request, response) {
+  console.log('In requestHandler'); // logs last if there is a request
   response.end(`Requested Path: ${request.url} \nRequest Method: ${request.method}`);
 };
 
 const server = http.createServer(requestHandler);
+console.log('Server created'); // logs 1st
 
 server.listen(PORT, () => {
-  console.log(`Server listening on: http://localhost:${PORT}`);
+  console.log(`Server listening on: http://localhost:${PORT}`); // logs 3rd
 });
 
+console.log('Last line (after .listen call)'); // logs 2nd
