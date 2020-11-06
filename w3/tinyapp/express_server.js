e88739ae7738f834fe7afcf6662f273577c4f70b
Sara Irving <sarakirving@gmail.com>
2020-11-06 17:55:48 +0000

    create route to render the urls_index template

--- w3/tinyapp/express_server.js
+++ w3/tinyapp/express_server.js
@@ -24,6 +24,11 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = {urls:  urlDatabase};
+  res.render("urls_index", templateVars);
+})
+
 // access via curl request in command line with: curl -i http://localhost:8080/hello
 
 app.listen(PORT, () => {
