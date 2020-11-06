662d2d912adb43fcdf9d2c9cf371a8f14e254105
Sara <sarakirving@gmail.com>
2020-11-05 18:55:17 -0800

    add comments for clarity and as examples of each step to explain to students

--- w3/tinyapp/express_server.js
+++ w3/tinyapp/express_server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port
 
+//sample of what data will look like
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -11,10 +12,12 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//get json format of urlDatabase data
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// send HTML in the response
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
