18007be6e72361e3251fa7212f07cb4ba878135d
Sara Irving <sarakirving@gmail.com>
2020-11-06 17:51:37 +0000

    set ejs as view engine

--- w3/tinyapp/express_server.js
+++ w3/tinyapp/express_server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port
 
+app.set("view engine", "ejs") //set ejs as the view engine 
+
 //sample of what data will look like
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
