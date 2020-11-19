881481e81d74be3078688ed50e8128a3b3328354
Sara Irving <sarakirving@gmail.com>
2020-11-19 19:26:01 +0000

    refactor code to make a binary search function

--- w4d5/array-search.js
+++ w4d5/array-search.js
@@ -12,3 +12,35 @@ function search(array, item) {
 
   return index;
 }
+
+// worst case scenario here is 3n + 4
+
+console.log(search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], -1));
+
+// refactor above code to make a binary search
+function binarySearch(array, item) {
+  let min = 0;
+  let max = array.length - 1;
+
+
+  while(true) {
+    let currentMid = Math.floor((min + max) / 2);
+    let currentItem = array[currentMid];
+
+    if (currentItem === item) {
+      return currentMid;
+    }
+
+    if (currentItem > item) {
+      max = currentMid - 1;
+    }
+
+    if (currentItem < item) {
+      min = currentMid + 1;
+    }
+
+    if(max < min) {
+      return null;
+    }
+  }
+}
