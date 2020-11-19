db48b2cc146077b8615ff96c99c9460597a02ad0
Sara Irving <sarakirving@gmail.com>
2020-11-19 20:23:16 +0000

    calculate run time of linear approach

--- w4d5/sum.js
+++ w4d5/sum.js
@@ -20,22 +20,22 @@ function arrayContainsSum(array, sum) {
 // Linear approach 
 function linearArrayContainsSum(array, sum) {
 
-  let i = 0;
-  let ii = array.length - 1;
+  let i = 0; // 1
+  let ii = array.length - 1; // 1
 
-  while(i <= ii) {
-    const element1 = array[i];
-    const element2 = array[ii];
-    const currentSum = element1 + element2 ;
+  while(i <= ii) { // n+1
+    const element1 = array[i]; //n
+    const element2 = array[ii]; //n
+    const currentSum = element1 + element2 ; //n
 
-    if(currentSum === sum) {
+    if(currentSum === sum) { //n
       return true;
-    } else if (currentSum > sum) {
-      ii--;
-    } else {
-      i++;
+    } else if (currentSum > sum) { //n
+      ii--; //n
+    } else { //n
+      i++; //n
     }
   }
-  return false;
-
+  return false; //1
 }
+//run time: 4 + 7n 
