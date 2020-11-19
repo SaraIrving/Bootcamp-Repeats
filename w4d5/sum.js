26345dcbec0dd0ceeecca868e50384fa4ee7ea6a
Sara Irving <sarakirving@gmail.com>
2020-11-19 20:07:13 +0000

    calculate worst case scenario run time

--- w4d5/sum.js
+++ w4d5/sum.js
@@ -1,15 +1,16 @@
 function arrayContainsSum(array, sum) {
 
-  for(let i = 0; i < array.length; i++) {
-    const element1 = array[i];
+  for(let i = 0; i < array.length; i++) { // 1  n+1  n
+    const element1 = array[i]; // n
 
-    for (let ii = 0; ii < array.length; ii++) {
-      const element2 = array[ii];
+    for (let ii = 0; ii < array.length; ii++) { // n   n=n^2  n^2
+      const element2 = array[ii]; // n^2
 
-      if (element1 + element2 === sum) {
+      if (element1 + element2 === sum) { //n^2
         return true;
       }
     }
   }
-  return false;
+  return false; // 1 
 }
+// run time: 3 + 5n + 4n^2
